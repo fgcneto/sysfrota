af7ce9cf1f5d52343d9fcedc9c963ffea4e157c7
fgcneto <fgcneto@gmail.com>
2022-05-27 18:45:22 -0300

    add models app Usuário

--- Usuario/models.py
+++ Usuario/models.py
@@ -1,17 +1,42 @@
+from enum import unique
 from django.db import models
 from django.contrib.auth.models import User
 from cpf_field.models import CPFField
 
+
 class Usuario(User):
-  
-  cpf = CPFField('cpf', unique=True)
+    cpf = CPFField('cpf', unique=True)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    class Meta:
+        ordering = ['first_name', 'last_name']
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"
 
 
-  def __str__(self):
-      return self.first_name + " " + self.last_name
+class Motorista(Usuario):
+    habilitacao = models.CharField(
+        max_length=20, null=True, blank=True, verbose_name='Habilitação')
 
-  class Meta:
-      ordering = ['first_name', 'last_name']
-      verbose_name = "Usuário"
-      verbose_name_plural = "Usuários"
+    def __str__(self):
+        return self.first_name + " " + self.last_name
 
+    class Meta:
+        ordering = ['first_name', 'last_name']
+        verbose_name = "Motorista"
+        verbose_name_plural = "Motoristas"
+
+
+class Administrador(Usuario):
+    matricula = models.CharField(
+        max_length=20, null=True, blank=True, verbose_name='Matrícula')
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    class Meta:
+        ordering = ['first_name', 'last_name']
+        verbose_name = "Administrador"
+        verbose_name_plural = "Administradores"
