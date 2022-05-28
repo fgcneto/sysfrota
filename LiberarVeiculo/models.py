038429e3c471d772f137f822538bd7ff58a4926c
fgcneto <fgcneto@gmail.com>
2022-05-28 17:17:10 -0300

    adição da função __str__ e class Meta

--- LiberarVeiculo/models.py
+++ LiberarVeiculo/models.py
@@ -1,4 +1,4 @@
-from pyexpat import model
+
 from django.db import models
 
 from Agenda.models import Agenda
@@ -13,9 +13,9 @@ class LiberarVeiculo(models.Model):
     observacoes = models.TextField(
         null=True, blank=True, verbose_name="Observações")
     agendamento = models.ForeignKey(
-        Agenda, on_delete=models.RESTRICT, related_name="agenda", verbose_name="Agenda")
+        Agenda, on_delete=models.RESTRICT, related_name="agenda_veiculo", verbose_name="Agenda")
     responsavel_liberacao = models.ForeignKey(
-        Administrador, on_delete=models.RESTRICT, related_name="administrador", verbose_name="Administrador")
+        Administrador, on_delete=models.RESTRICT, related_name="administrador_veiculo", verbose_name="Administrador")
     porteiro_saida = models.ForeignKey(
         Porteiro, on_delete=models.RESTRICT, related_name="porteiro_saida", verbose_name="Porteiro")
     porteiro_chegada = models.ForeignKey(
@@ -24,7 +24,15 @@ class LiberarVeiculo(models.Model):
         verbose_name="Assinatura do Porteiro na Saída")
     assinatura_motorista_chegada = models.BooleanField(
         verbose_name="Assinatura do Porteiro na Chegada")
-    km_saida = models.IntegerField(
+    km_saida = models.PositiveIntegerField(
         verbose_name="Kilometragem de Saída")
-    km_chegada = models.ImageField(
+    km_chegada = models.PositiveIntegerField(
         verbose_name="Kilometragem de Chegada")
+
+    def __str__(self):
+        return str(self.agendamento) + " - " + str(self.responsavel_liberacao) + " - " + str(self.agendamento.motorista) + " - " + str(self.agendamento.veiculo) + " - " + str(self.agendamento.hora_saida)
+
+    class Meta:
+        ordering = ['agendamento', 'responsavel_liberacao']
+        verbose_name = "Liberar Veículo"
+        verbose_name_plural = "Liberar Veículos"
