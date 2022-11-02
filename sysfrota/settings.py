6b86a11391d8e6dcc1ecbac3f1d0be8b582a11f8
fgcneto <fgcneto@gmail.com>
2022-11-01 22:25:50 -0300

    code

--- sysfrota/settings.py
+++ sysfrota/settings.py
@@ -92,17 +92,26 @@ WSGI_APPLICATION = 'sysfrota.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "postgres",
+#         "USER": "postgres",
+#         "PASSWORD": "postgres",
+#         "HOST": "db",
+#         "PORT": 5432,
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "db",
-        "PORT": 5432,
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': [database from heroku postgres],
+        'USER': [user from heroku postgres],
+        'PASSWORD': [password from heroku postgres],
+        'HOST': [host from heroku postgres],
+        'PORT':  [Port from heroku postgres]
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
